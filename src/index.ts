8b82dec9859633d5b1375e5dc9dddc988b17a2de
İsmail Karslı <ismail@karsli.net>
2025-10-31 12:54:43 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+type Handler = (topic: string, msg: ArrayBufferLike) => void;
+
+interface TrieNode {
+  children: Record<string, TrieNode>;
+  handlers?: Handler[];
+}
+
+export class MqttTrie {
+  root: TrieNode = { children: {} };
+
+  add(pattern: string, handler: Handler) {
+    let node = this.root;
+    for (const part of pattern.split("/")) {
+      node.children[part] = node.children[part] || { children: {} };
+      node = node.children[part];
+    }
+    node.handlers = node.handlers || [];
+    node.handlers.push(handler);
+  }
+
+  match(topic: string) {
+    const result: Handler[] = [];
+    const levels = topic.split("/");
+
+    const search = (node: TrieNode, i: number) => {
+      if (node.handlers && i === levels.length) result.push(...node.handlers);
+      if (i >= levels.length) return;
+      const part = levels[i];
+      if (!part) return;
+      if (node.children[part]) search(node.children[part], i + 1);
+      if (node.children["+"]) search(node.children["+"], i + 1);
+      if (node.children["#"]?.handlers)
+        result.push(...node.children["#"].handlers);
+    };
+
+    search(this.root, 0);
+    return result;
+  }
+}
